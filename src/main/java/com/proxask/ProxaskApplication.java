fdbecdc057785d694cea3cf10046fccb54870e58
PRINCE RAJ <rajprince031@gmail.com>
2025-07-18 00:02:56 +0530

    Initial commit

---
+++ src/main/java/com/proxask/ProxaskApplication.java
@@ -0,0 +1,13 @@
+package com.proxask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxaskApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/proxask/entity/User.java
@@ -0,0 +1,4 @@
+package com.proxask.entity;
+
+public class User {
+}
